4c53e0dc1cabcb49c6565d7334fd712bd8c18b5f
Szymon <djsmall@poczta.fm>
2013-02-04 20:57:08 +0100

    program

---
+++ Kopiowanie plików/src/kopiowanie/plików/DelayApp.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kopiowanie.plików;
+
+/**
+ *
+ * @author Szymon
+ */
+public class DelayApp implements Runnable{
+    
+    int delay;
+    MoveFile moveApp;
+    private javax.swing.JTextArea jTextArea1=null;
+    
+    public DelayApp (String sourceDir, String destDir, int delay) {
+        this.moveApp = new MoveFile( sourceDir,destDir);
+        this.delay = delay;
+    }
+    
+    public DelayApp (String sourceDir, String destDir, int delay, javax.swing.JTextArea jTextArea1) {
+        this.moveApp = new MoveFile( sourceDir,destDir);
+        this.delay = delay;
+        this.jTextArea1 = jTextArea1;
+    }
+    
+    public void runApp (){   
+        jTextArea1.append(Integer.toString(moveApp.MoveFiles()));
+    }
+
+    @Override
+    public void run() {
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ Kopiowanie plików/src/kopiowanie/plików/MoveFile.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kopiowanie.plików;
+
+import java.io.File;
+
+/**
+ *
+ * @author Szymon
+ */
+public class MoveFile {
+    
+    String sourceDir;
+    String destDir;
+    
+    File trDir;
+    File tsDir;
+    
+    public MoveFile(String sourceDir, String destDir ) {
+        this.sourceDir = sourceDir;
+        this.destDir = destDir;
+        
+        trDir = new File(sourceDir);
+        tsDir = new File(destDir);     
+    }
+    
+    public int MoveFiles() {
+        int count = trDir.listFiles().length;
+        for(int i = 0; i<trDir.listFiles().length; ) {
+            File temp = trDir.listFiles()[i];
+            if(!temp.isDirectory()) {
+                File tempCopy = new File(tsDir + "/" + temp.getName());
+                tempCopy.delete();
+                temp.renameTo(tempCopy);
+            }
+            System.out.print(temp.getName());
+            System.out.println(i);
+        }
+        return count;
+    }
+
+}
